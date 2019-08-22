218d32f168d925e250fa7ae9d47af42962ce7607
dengquan <dengquan@qq.cn>
2019-08-22 23:24:09 +0800

    样式的引入和首页的搭建

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,49 @@
 <template>
   <div id="app">
+    <!-- 头部 -->
+    <van-nav-bar title="我的影院" left-text="返回" left-arrow @click-left="onClickLeft" />
+
+    <!-- 内容 -->
     <router-view />
+
+    <!-- 底部 -->
+    <van-tabbar v-model="active" active-color="rgb(255, 83, 83)">
+      <van-tabbar-item icon="home-o">猫眼电影</van-tabbar-item>
+      <van-tabbar-item icon="search">猫眼城市</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">个人中心</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      active: 0
+    };
+  },
+  methods: {
+    onClickLeft() {
+      console.log("返回");
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+#app {
+  .van-nav-bar {
+    background-color: rgb(255, 83, 83);
+    .van-nav-bar__left {
+      .van-icon {
+        color: #fff;
+      }
+      .van-nav-bar__text {
+        color: #fff;
+      }
+    }
+    .van-nav-bar__title {
+      color: #fff;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './routers'
-import store from './stores'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './routers';
+import store from './stores';
 
-Vue.config.productionTip = false
+// vant组件的引入
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,

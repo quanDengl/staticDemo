a727ddd44b9be2d03dc1fcf2ff1d3d9971d15472
dengquan <dengquan@qq.cn>
2019-12-15 21:39:24 +0800

    合并

--- src/views/movie/detail.vue
+++ src/views/movie/detail.vue
@@ -5,68 +5,34 @@
     </headers>
     <div id="content" class="contentDetail">
       <div class="detail_list">
-        <div class="detail_list_bg"></div>
+        <div class="detail_list_bg" :style="{ 'background-image' : 'url('+ detailMovieImg +')' }"></div>
         <div class="detail_list_filter"></div>
         <div class="detail_list_content">
           <div class="detail_list_img">
-            <img src="/images/movie_1.jpg" alt />
+            <!-- <img :src="detailMovie.img.replace(/w\.h/,'108.150')" alt /> -->
+            <img :src="detailMovieImg" alt />
           </div>
           <div class="detail_list_info">
-            <h2>无名之辈</h2>
-            <p>A Cool Fish</p>
-            <p>9.2</p>
-            <p>剧情,喜剧,犯罪</p>
-            <p>中国大陆 / 108分钟</p>
-            <p>2018-11-16大陆上映</p>
+            <h2>{{detailMovie.nm}}</h2>
+            <p>{{detailMovie.enm}}</p>
+            <p>{{detailMovie.sc}}</p>
+            <p>{{detailMovie.cat}}</p>
+            <p>{{detailMovie.src}} / {{detailMovie.dur}}分钟</p>
+            <p>{{detailMovie.pubDesc}}</p>
           </div>
         </div>
       </div>
       <div class="detail_intro">
-        <p>在一座山间小城中，一对低配劫匪、一个落魄的泼皮保安、一个身体残疾却性格彪悍的残毒舌女以及一系列生活在社会不同轨迹上的小人物，在一个貌似平常的日子里，因为一把丢失的老枪和一桩当天发生在城中的乌龙劫案，从而被阴差阳错地拧到一起，发生的一幕幕令人啼笑皆非的荒诞喜剧。</p>
+        <p>{{detailMovie.dra}}</p>
       </div>
-      <div class="detail_player swiper-container">
+      <div class="detail_player swiper-container" ref="detail_player">
         <ul class="swiper-wrapper">
-          <li class="swiper-slide">
+          <li class="swiper-slide" v-for="(item,index) in detailMovie.photos" :key="index">
             <div>
-              <img src="/images/person_1.webp" alt />
+              <img :src="item | setWH('140.127')" alt />
             </div>
-            <p>陈建斌</p>
-            <p>马先勇</p>
-          </li>
-          <li class="swiper-slide">
-            <div>
-              <img src="/images/person_1.webp" alt />
-            </div>
-            <p>陈建斌</p>
-            <p>马先勇</p>
-          </li>
-          <li class="swiper-slide">
-            <div>
-              <img src="/images/person_1.webp" alt />
-            </div>
-            <p>陈建斌</p>
-            <p>马先勇</p>
-          </li>
-          <li class="swiper-slide">
-            <div>
-              <img src="/images/person_1.webp" alt />
-            </div>
-            <p>陈建斌</p>
-            <p>马先勇</p>
-          </li>
-          <li class="swiper-slide">
-            <div>
-              <img src="/images/person_1.webp" alt />
-            </div>
-            <p>陈建斌</p>
-            <p>马先勇</p>
-          </li>
-          <li class="swiper-slide">
-            <div>
-              <img src="/images/person_1.webp" alt />
-            </div>
-            <p>陈建斌</p>
-            <p>马先勇</p>
+            <!-- <p>陈建斌</p>
+            <p>马先勇</p>-->
           </li>
         </ul>
       </div>
@@ -81,13 +47,33 @@ export default {
       import(/* webpackChunkName: 'CommonBase' */ "@/components/headers")
   },
   props: ["movieId"],
+  data() {
+    return {
+      detailMovie: {},
+      detailMovieImg: ''
+    };
+  },
   methods: {
     handleToBack() {
       this.$router.back();
     }
   },
   mounted() {
-    console.log(this.movieId);
+    // console.log(this.movieId);
+    this.axios.get("/api/detailmovie?movieId=" + this.movieId).then(res => {
+      var msg = res.data.msg;
+      if (msg === "ok") {
+        this.detailMovie = res.data.data.detailMovie;
+        this.detailMovieImg = res.data.data.detailMovie.img.replace(/w\.h/, ('108.150'));
+        this.$nextTick(() => {
+          new Swiper(this.$refs.detail_player, {
+            slidesPerView: "auto",
+            freeMode: true,
+            freeModeSticky: true
+          });
+        });
+      }
+    });
   }
 };
 </script>
@@ -125,7 +111,7 @@ export default {
 .detail_list .detail_list_bg {
   width: 100%;
   height: 100%;
-  background: url(/images/movie_1.jpg) 0 40%;
+  /* background: url(/images/movie_1.jpg) 0 40%; */
   filter: blur(20px);
   background-size: cover;
   position: absolute;
@@ -176,5 +162,26 @@ export default {
   line-height: 20px;
   font-size: 14px;
   color: #ccc;
+}
+#content .detail_intro {
+  padding: 10px;
+}
+#content .detail_player {
+  margin: 20px;
+}
+.detail_player .swiper-slide {
+  width: 70px;
+  height: 50px;
+  margin-right: 20px;
+  text-align: center;
+  font-size: 14px;
+}
+.detail_player .swiper-slide img {
+  width: 70px;
+  height: 50px;
+  margin-bottom: 5px;
+}
+.detail_player .swiper-slide p:nth-of-type(2) {
+  color: #999;
 }
 </style>
